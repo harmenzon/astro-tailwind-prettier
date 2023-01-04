c4b595081696f7bcd14f853c057ef3d8d3d15af9
Martijn Harmenzon <martijn.harmenzon@maptm.nl>
2023-01-04 10:46:32 +0100

    First commit

---
+++ frontend/prettier.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: [
+    require.resolve("prettier-plugin-astro"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+  tailwindConfig: "./tailwind.config.cjs",
+  pluginSearchDirs: false,
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+    {
+      files: "*.js",
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
+};
